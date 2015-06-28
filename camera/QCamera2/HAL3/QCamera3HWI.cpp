82bb32df723ed59ba679dd8406064e6c81e3c41b
Jignesh Mehta <jigneshm@codeaurora.org>
2015-06-27 19:04:59 -0700

    QCamera2: Migrate daemon process to mediaserver
    - mci will open and call mct shimlayer APIs.
    - Domain socket connection is removed.
    - Virtual address of hal pointer will be assigned to
      MCT image buffer list.
    - For VB2 operation, it will call to camera kernel.
    
    CRs-Fixed: 1005685
    Change-Id: I2e3edc47148ea2aefc201057695f08ceb4ec070a

--- camera/QCamera2/HAL3/QCamera3HWI.cpp
+++ camera/QCamera2/HAL3/QCamera3HWI.cpp
@@ -5954,7 +5954,8 @@ int QCamera3HardwareInterface::initCapabilities(uint32_t cameraId)
     rc = cameraHandle->ops->map_buf(cameraHandle->camera_handle,
                                 CAM_MAPPING_BUF_TYPE_CAPABILITY,
                                 capabilityHeap->getFd(0),
-                                sizeof(cam_capability_t));
+                                sizeof(cam_capability_t),
+                                capabilityHeap->getPtr(0));
     if(rc < 0) {
         LOGE("failed to map capability buffer");
         goto map_failed;
@@ -6048,7 +6049,8 @@ int QCamera3HardwareInterface::initParameters()
     rc = mCameraHandle->ops->map_buf(mCameraHandle->camera_handle,
             CAM_MAPPING_BUF_TYPE_PARM_BUF,
             mParamHeap->getFd(0),
-            sizeof(metadata_buffer_t));
+            sizeof(metadata_buffer_t),
+            (metadata_buffer_t *) DATA_PTR(mParamHeap,0));
     if(rc < 0) {
         LOGE("failed to map SETPARM buffer");
         rc = FAILED_TRANSACTION;
