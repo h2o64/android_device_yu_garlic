855639796887117e05261807a4400619582660d6
Patrick Tjin <pattjin@google.com>
2016-03-04 09:18:40 -0800

    garlic: camera: add EXTRA_POWERHAL_HINTS guard around usage of POWER_HINT_CAM_PREVIEW
    Bug: 27456444
    
    Change-Id: I263d2fc5bb3d35402ff2ecb752bfde7e4b8a0be8
    Signed-off-by: Patrick Tjin <pattjin@google.com>

--- camera/QCamera2/HAL/QCamera2HWI.cpp
+++ camera/QCamera2/HAL/QCamera2HWI.cpp
@@ -411,8 +411,10 @@ void QCamera2HardwareInterface::stop_preview(struct camera_device *device)
     LOGI("[KPI Perf]: E PROFILE_STOP_PREVIEW camera id %d",
              hw->getCameraId());
 
+#ifdef EXTRA_POWERHAL_HINTS
     // Disable power Hint for preview
     hw->m_perfLock.powerHint(POWER_HINT_CAM_PREVIEW, false);
+#endif
 
     hw->m_perfLock.lock_acq();
     hw->lockAPI();
@@ -3468,10 +3470,12 @@ int QCamera2HardwareInterface::startPreview()
     }
     m_perfLock.lock_rel();
 
+#ifdef EXTRA_POWERHAL_HINTS
     if (rc == NO_ERROR) {
         // Set power Hint for preview
         m_perfLock.powerHint(POWER_HINT_CAM_PREVIEW, true);
     }
+#endif
 
     LOGI("X rc = %d", rc);
     return rc;
@@ -3502,8 +3506,10 @@ int QCamera2HardwareInterface::stopPreview()
     mNumPreviewFaces = -1;
     mActiveAF = false;
 
+#ifdef EXTRA_POWERHAL_HINTS
     // Disable power Hint for preview
     m_perfLock.powerHint(POWER_HINT_CAM_PREVIEW, false);
+#endif
 
     m_perfLock.lock_acq();
 
