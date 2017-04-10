8cb51d33109c4abf426760911bc5145a66722d0f
TheScarastic <warabhishek@gmail.com>
2017-04-10 17:38:52 +0000

    garlic: Import Initial keylayouts

--- device.mk
+++ device.mk
@@ -82,6 +82,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
