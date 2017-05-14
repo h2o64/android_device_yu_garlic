628ef24cc9f5261db05733d92418b31a2afe83de
Louis Popi <theh2o64@gmail.com>
2017-05-14 07:37:02 +0200

    gps: Include LOCAL_PATH

--- gps/Android.mk
+++ gps/Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,4 +14,8 @@
 # limitations under the License.
 #
 
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),garlic)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
