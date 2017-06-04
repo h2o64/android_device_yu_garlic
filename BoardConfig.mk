f714a04789b59b90b73a81786030fe07586c07fc
Louis Popi <theh2o64@gmail.com>
2017-06-04 12:02:06 +0200

    garlic: Cleanup mkbootimg args

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,11 +43,9 @@ TARGET_USES_64_BIT_BINDER := true
 BOARD_KERNEL_CMDLINE := g_android.luns=1 g_android.removable=1 g_android.cdrom=1 console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS += --kernel_offset 0x00008000
-BOARD_MKBOOTIMG_ARGS += --ramdisk_offset 0x01000000
-BOARD_MKBOOTIMG_ARGS += --tags_offset 0x00000100
-BOARD_MKBOOTIMG_ARGS += --second_offset 0x80f00000
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_CONFIG := lineageos_garlic_defconfig
 TARGET_KERNEL_SOURCE := kernel/yu/msm8937
 
