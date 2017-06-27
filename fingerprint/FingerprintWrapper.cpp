00210cf41afca98344b956957e0a5bede7e051e6
Louis Popi <theh2o64@gmail.com>
2017-06-27 17:28:17 +0200

    garlic: fingerprint: Let's make goodix the fallback option

--- fingerprint/FingerprintWrapper.cpp
+++ fingerprint/FingerprintWrapper.cpp
@@ -60,20 +60,14 @@ static bool ensure_vendor_module_is_loaded(void)
         property_set("sys.fingerprintd", "1");
         if (strcmp(vend, "elan_fp")) {
             rv = hw_get_module_by_class("fingerprint", "elan", &vendor.hw_module);
-        } else if (strcmp(vend, "goodix_fp")) {
-            property_set("persist.sys.fp.goodix", "1");
-            rv = hw_get_module_by_class("fingerprint", "goodix", &vendor.hw_module);
-        } else if (strcmp(vend, "silead_fp_dev")) {
-            ALOGE("Silead fpsvcd fingerprint sensor is unsupported");
-            vendor.module = NULL;
         } else {
             if (rv) {
                 ALOGE("failed to open vendor module, error %d", rv);
                 vendor.module = NULL;
             } else {
-                ALOGI("loaded vendor module: %s version %x", vendor.module->common.name,
-                vendor.module->common.module_api_version);
-            }
+            	property_set("persist.sys.fp.goodix", "1");
+            	rv = hw_get_module_by_class("fingerprint", "goodix", &vendor.hw_module);
+						}
         }
     }
 
