908f6c4756c245afc5773ac273fde5744c32aab8
Louis Popi <theh2o64@gmail.com>
2017-06-28 22:43:08 +0200

    garlic: Change the ims external libs path
    * /system/lib* not /system/vendor/lib*

--- Android.mk
+++ Android.mk
@@ -24,13 +24,12 @@ include device/yu/garlic/tftp.mk
 
 include $(CLEAR_VARS)
 IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
-
 IMS_SYMLINKS := $(addprefix $(TARGET_OUT)/priv-app/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
 $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "IMS lib link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf /system/vendor/lib64/$(notdir $@) $@
+	$(hide) ln -sf /system/lib64/$(notdir $@) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
 
