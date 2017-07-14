a114a80a75be22c42c589499eef217d0dc3567e8
Louis Popi <theh2o64@gmail.com>
2017-07-14 07:17:28 +0200

    init: Simplify reading mehtod

--- init/init_garlic.cpp
+++ init/init_garlic.cpp
@@ -28,52 +28,26 @@
 #include "util.h"
 #include "vendor_init.h"
 
-#define STRCONV_(x)      #x
-#define STRCONV(x) "%" STRCONV_(x) "s"
-#define BUF_SIZE 64
 #define DRV_INFO "/sys/devices/platform/fp_drv/fp_drv_info"
-
-static int read_file2(const char *fname, char *data, int max_size)
-{
-    int fd, rc;
-
-    if (max_size < 1)
-        return 0;
-
-    fd = open(fname, O_RDONLY);
-    if (fd < 0) {
-        ERROR("failed to open '%s'\n", fname);
-        return 0;
-    }
-
-    rc = read(fd, data, max_size -1);
-    if ((rc > 0) && (rc < max_size ))
-        data[rc] = '\0';
-    else
-        data[0] = '\0';
-    close(fd);
-
-    return 1;
-}
 
 static void fp_prop()
 {
-    int rv;
-    char buf[BUF_SIZE];
-    char vend[BUF_SIZE];
+    int fd = open(DRV_INFO, 0);
+    if (fd <= 0) {
+        ERROR("Cannot open: %s", DRV_INFO);
+    }
 
-    if (read_file2(DRV_INFO, buf, sizeof(buf)))
-        sscanf(buf, STRCONV(BUF_SIZE), vend);
-    else
-        ERROR("%s: Failed to open %c\n", __func__, DRV_INFO);
+    char fp_drv[50];
+    memset(fp_drv, 0, sizeof(fp_drv));
+    int result = read(fd, fp_drv, sizeof(fp_drv));
 
-    if (!strcmp(vend, "elan_fp")) {
+    if (strcmp(fp_drv, "elan_fp") == 0) {
         property_set("ro.hardware.fingerprint", "elan");
         property_set("persist.sys.fp.goodix", "0");
-    } else if (!strcmp(vend, "goodix_fp")) {
+    } else if (strcmp(fp_drv, "goodix_fp") == 0) {
         property_set("ro.hardware.fingerprint", "goodix");
         property_set("persist.sys.fp.goodix", "1");
-    } else if (!strcmp(vend, "silead_fp_dev")) {
+    } else if (strcmp(fp_drv, "silead_fp_dev") == 0) {
         ERROR("%s: Silead fpsvcd fingerprint sensor is unsupported", __func__);
     } else {
         ERROR("%s: Fingerprint sensor is unsupported", __func__);
