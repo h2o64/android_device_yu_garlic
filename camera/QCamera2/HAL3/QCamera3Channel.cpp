c023d0eb509cfe1f6d6828ea32f1b825f25d41ed
Suman Mukherjee <sumam@codeaurora.org>
2017-11-30 14:50:06 +0530

    garlic: QCamera2: Fixed compilation error for AOSP upgrade
      * Fixed compilation error for O
    
    Change-Id: I904ad9b6f6fa05a0b50816daada20944567d08bc
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- camera/QCamera2/HAL3/QCamera3Channel.cpp
+++ camera/QCamera2/HAL3/QCamera3Channel.cpp
@@ -39,6 +39,8 @@
 #include <stdlib.h>
 #include "hardware/gralloc.h"
 #include <utils/Timers.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 // Camera dependencies
 #include "QCamera3Channel.h"
