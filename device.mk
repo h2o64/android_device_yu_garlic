c273bd9c4ecacaa606ce83eb92a845823ca3fe2e
Ramjee Singh <ramjee@codeaurora.org>
2017-12-28 15:13:39 +0530

    garlic: Move flac decoder to vendor specific xml file
    - Moved FlacDecoder to a new vendor specific xml file
      to avoid VTS test case failures on vanilla system
      image.
    
    Change-Id: Ie3718281a237b7aea94b2380a564da1a5bb15f79

--- device.mk
+++ device.mk
@@ -228,6 +228,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/configs/media_codecs_vendor_audio.xml:system/etc/media_codecs_vendor_audio.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 PRODUCT_COPY_FILES += \
