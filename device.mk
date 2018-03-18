5c644bbd1e3c383cad6cd17080a9a73a52f00813
Bruno Martins <bgcngm@gmail.com>
2018-03-18 21:20:20 +0000

    garlic: Remove non-existing HAL services

--- device.mk
+++ device.mk
@@ -84,10 +84,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service \
     android.hardware.broadcastradio@1.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
-    android.hardware.soundtrigger@2.0-service
+    android.hardware.soundtrigger@2.0-impl
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
