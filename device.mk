d24cfa84405c6fbfb3a9b3d85d20e3052b589bcd
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2018-03-24 08:52:17 +0000

    garlic: Add Goodix gt9xx keylayout
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- device.mk
+++ device.mk
@@ -268,6 +268,7 @@ PRODUCT_PACKAGES += \
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
+    $(LOCAL_PATH)/keylayout/goodix-ts.kl:system/usr/keylayout/goodix-ts.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/TPFT5XX_GESTURE.kl:system/usr/keylayout/TPFT5XX_GESTURE.kl
 
