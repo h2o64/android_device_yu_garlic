e9db7cadb541daa00cf0ef7bc270be4c50e45bde
Louis Popi <theh2o64@gmail.com>
2018-06-24 09:42:23 +0200

    Revert "garlic: Use binderised camera"
    This reverts commit d741b1dbf960480168891ebbc1c0283a9f06c9fd.

--- device.mk
+++ device.mk
@@ -113,6 +113,11 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-service
 
 # Camera
+PRODUCT_PACKAGES += \
+    camera.msm8937 \
+    libshim_libmmcamera \
+    Snap
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/camera_config.xml:system/etc/camera/camera_config.xml \
     $(LOCAL_PATH)/configs/camera/csidtg_camera.xml:system/etc/camera/csidtg_camera.xml \
@@ -122,11 +127,8 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
-    camera.msm8937 \
     libmm-qcamera \
-    libshim_libmmcamera \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
@@ -332,10 +334,6 @@ PRODUCT_COPY_FILES += \
 
 TARGET_LD_SHIM_LIBS += \
     /system/vendor/lib/libmmcamera_ppeiscore.so|libshim_libmmcamera.so
-
-# Snap
-PRODUCT_PACKAGES += \
-    Snap
 
 # Thermal HAL
 PRODUCT_PACKAGES += \
