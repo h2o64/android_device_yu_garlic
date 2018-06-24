10120274751de9e557f65fb65e1a21a3cb6e4068
Louis Popi <theh2o64@gmail.com>
2018-06-24 09:41:01 +0200

    Revert "garlic: Remove shims"
    This reverts commit c4b3823e4c6497a830bdbce83d8ca8d1b9f30b48.

--- device.mk
+++ device.mk
@@ -330,6 +330,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
+TARGET_LD_SHIM_LIBS += \
+    /system/vendor/lib/libmmcamera_ppeiscore.so|libshim_libmmcamera.so
+
 # Snap
 PRODUCT_PACKAGES += \
     Snap
